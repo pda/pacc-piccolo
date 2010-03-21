605d98052bc237d7e485e4a1578420ad45898b38
Paul Annesley <paul@annesley.cc>
2010-03-22 01:38:52 +1100

    Shoddy import script for posts from old CouchDB database.

---
+++ import-couchdb.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'json'
+
+JSON.parse(File.read('posts.couchdb'))['rows'].map{|r| r['value']}.each do |row|
+  File.open("posts/#{row['_id']}.txt", 'w') do |f|
+    f.puts '---'
+    if row['title'].match(/: /)
+      f.puts "title: \"#{row['title']}\""
+    else
+      f.puts "title: #{row['title']}"
+    end
+    f.puts "date: #{row['timecreated']}"
+    f.puts "modified: #{row['timemodified']}" if row['timemodified'] != row['timecreated']
+    f.puts 'uid: ' + row['uid']
+    f.puts
+    f.puts row['content'].gsub("\r\n", "\n")
+  end
+end
