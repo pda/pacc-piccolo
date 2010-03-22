f9209dbea35674bb178d0d2767e5ba451066e3fc
Paul Annesley <paul@annesley.cc>
2010-03-22 19:48:48 +1100

    Atom feed; needs tidying.

--- piccolo.rb
+++ piccolo.rb
@@ -1,4 +1,4 @@
-%w{ rubygems yaml haml rdiscount }.each{ |r| require r }
+%w{ rubygems yaml haml rdiscount builder }.each{ |r| require r }
 
 module Piccolo
 
@@ -30,6 +30,35 @@ module Piccolo
         posts, links = PostCollection.new, LinkCollection.new
         entries = (posts.to_a + links.to_a).sort
         response.body = HamlView.new(:home, :entries => entries).to_html
+
+      # feed
+      elsif %w{ /articles/feed /feed /feed/atom /feed/ }.member? request.path
+        posts, links = PostCollection.new, LinkCollection.new
+        entries = (posts.to_a + links.to_a).sort
+
+        # thanks to http://github.com/cloudhead/toto
+        feed = Builder::XmlMarkup.new :indent => 2
+        feed.instruct!
+        feed.feed "xmlns" => "http://www.w3.org/2005/Atom" do
+          feed.title 'paul.annesley.cc'
+          feed.id 'http://paul.annesley.cc/'
+          feed.link :rel => 'self', :href => 'http://paul.annesley.cc/feed'
+          feed.link :rel => 'alternate', :href => 'http://paul.annesley.cc/'
+          feed.updated Time.parse(posts.first.date.asctime).gmtime.iso8601 unless posts.empty?
+          feed.author { feed.name 'Paul Annesley' }
+
+          posts.each do |post|
+            feed.entry do
+              feed.id post.meta['uid']
+              feed.published Time.parse(post.date.asctime).gmtime.iso8601
+              feed.title post.title
+              feed.link 'rel' => 'alternate', 'href' => 'http://paul.annesley.cc' + post.url
+              feed.content post.content, "type" => "html"
+            end
+          end
+        end
+        response['Content-Type'] = 'application/atom+xml'
+        response.body = feed.to_xml
 
       # page
       elsif /^\/([\w-]+)$/.match(request.path)
@@ -123,6 +152,9 @@ module Piccolo
     DIR = 'posts'
     def each
       Dir.glob("#{DIR}/*.txt").reverse.each { |path| yield Post.new(path) }
+    end
+    def empty?
+      to_a.empty?
     end
     def post(year, month, stub)
       Post.new('%s/%04d-%02d-%s.txt' % [DIR, year, month, stub])
